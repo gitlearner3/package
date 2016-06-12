ada96086ce264a5cfcca7cbd3630b78bf97a3a28
Git Learner <gitlearner3@yahoo.com>
2016-06-12 10:40:21 -0400

    main nad stringutil

---
+++ main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "github.com/gitlearner3/package/stringutil"
+)
+
+func main() {
+    fmt.Println(stringutil.Reverse("!oG ,olleH"))
+    fmt.Println(stringutil.MyName)
+}
